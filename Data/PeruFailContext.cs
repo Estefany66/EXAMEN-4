30a66810298a1f2f55947169cee813848e94fad2
Estefany Calatayud <estefany_calatayud@usmp.pe>
2021-06-15 20:42:20 -0500

    .

--- Data/PeruFailContext.cs
+++ Data/PeruFailContext.cs
@@ -1,11 +1,11 @@
 using Microsoft.EntityFrameworkCore;
-using PC_4.Models;
+using EXAMEN_4.Models;
 
 namespace EXAMEN_4.Models
 {
     public class PeruFailContext: DbContext
     {
-        public DbSet<Usuario> Usuarios {get; set;}
+        public DbSet<Cliente> Clientes {get; set;}
         public DbSet<Visita> Visitas {get; set;}
    
    public PeruFailContext(DbContextOptions dco) : base(dco){

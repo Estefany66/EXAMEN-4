2c4e00a92985b72034b178778201567864d42941
Estefany Calatayud <estefany_calatayud@usmp.pe>
2021-06-15 19:05:28 -0500

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EXAMEN_4.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
